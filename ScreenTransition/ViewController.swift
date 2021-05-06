3cf006473272e6a51187f82c5f1b6ec38657222e
Tomoyoshi Masuyama <brucemasu@gmail.com>
2021-05-06 19:28:02 +0900

    Initial Commit

---
+++ ScreenTransition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenTransition
+//
+//  Created by 益山知慶 on 2021/05/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
